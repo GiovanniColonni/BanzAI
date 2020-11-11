4472fda6002e39e017557c52bb0c235991b057f4
GiovanniColonni <giovacolo97@gmail.com>
2020-11-11 22:20:50 +0100

    scrittura su cvs dei dati

--- ProvaGoogleStat.py
+++ ProvaGoogleStat.py
@@ -1,18 +1,29 @@
 from pytrends.request  import TrendReq
 import pandas as pd
-import csv
+from pandas import DataFrame
+import csv as cvs
 
-film_list = ["La Casa di Carta","Il trono di spade","La grande bellezza"]
+film_list = ["Il trono di spade"]
+time_frame = "2020-10-11 2020-11-11"
+## Dataset layout :
+                                    #Regione1 #Regione2
+            #<numero trimestre,Anno> 
+    # FILM  #<numero trimestre,Anno>
+            #<numero trimestre,Anno>
 
-
-
+def write_to_cvs(data_frame):
+    with open("film.cvs","w",newline='') as file:
+        file = cvs.writer(file)
+        for i in range(0,data_frame.size):
+            file.writerow([film_list[0],data_frame.index[i],data_frame.values[i],time_frame])
+        
 
 def main():
-    #pyT = TrendReq(hl="it-IT")
-    pyT = TrendReq()
-    pyT.build_payload(kw_list=["Il Trono di Spade"],timeframe="2020-11-11",geo="IT")
+    pyT = TrendReq(hl="it-IT")
+    #pyT = TrendReq()
+    pyT.build_payload(kw_list=film_list,timeframe=time_frame,geo="IT")
     data_frame = pyT.interest_by_region()
-    print(data_frame)
+    write_to_cvs(data_frame)   
 
     
 if __name__ == "__main__":
